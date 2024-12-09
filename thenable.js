c4766a873a2a5dfc6d8f90558376694d77017186
ayush931 <ayushkumar9315983@gmail.com>
2024-12-09 12:15:19 +0530

    Thenable

---
+++ thenable.js
@@ -0,0 +1,15 @@
+const thenable = {
+	then: function(onFulfilled) {
+		setTimeout(() => onFulfilled(42), 1000)
+	}
+}
+
+async function main () {
+	await thenable
+	console.log("Hii there")
+}
+
+main ()
+
+// Every promise is a thenable
+// Every thenable is not a promise
