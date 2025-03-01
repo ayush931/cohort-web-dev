70fb784db8d313d35d86e0558cc43b33fcea843f
ayush931 <ayushkumar9315983@gmail.com>
2025-03-01 10:27:17 +0530

    Sidebar Functionality completed

--- design/src/App.jsx
+++ design/src/App.jsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoReorderThree } from "react-icons/io5";
 
-function App() {
+const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
 
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    const listener = () => setMatches(media.matches);
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [query]);
+
+  return matches;
+};
+
+function App() {
   const [sideBarOpen, setSideBarOpen] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  useEffect(() => {
+    if (isDesktop == false) {
+      setSideBarOpen(false);
+    } else {
+      setSideBarOpen(true);
+    }
+  }, [isDesktop]);
 
   return (
     <div className="flex">
@@ -13,48 +34,48 @@ function App() {
   );
 }
 
-function SideBar ({ sideBarOpen, setSideBarOpen }) {
+function SideBar({ sideBarOpen, setSideBarOpen }) {
   if (!sideBarOpen) {
     return (
-      <div className="fixed top-1 left-1">
-        <div className="cursor-pointer hover:bg-slate-200" onClick={() => {
-          setSideBarOpen(!sideBarOpen)
-        }}>
-          <IoReorderThree className="text-white" />
+      <div className="fixed top-0 left-0">
+        <div
+          className="cursor-pointer hover:bg-slate-200"
+          onClick={() => {
+            setSideBarOpen(!sideBarOpen);
+          }}
+        >
+          <IoReorderThree />
         </div>
       </div>
-    )
+    );
   }
   return (
-    <div className="w-96 h-screen bg-red-200">
+    <div className="w-96 h-screen bg-red-200 fixed top-0 left-0 md:relative">
       <div>
-        <div className="cursor-pointer hover:bg-slate-200" onClick={() => {
-          setSideBarOpen(!sideBarOpen)
-        }}>
+        <div
+          className="cursor-pointer hover:bg-slate-200"
+          onClick={() => {
+            setSideBarOpen(!sideBarOpen);
+          }}
+        >
           <IoReorderThree />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-function MainContent () {
+function MainContent() {
   return (
     <div className="w-full">
-      <div className="h-72 bg-black"></div>
+      <div className="h-72 bg-black hidden md:block"></div>
       <div className="grid grid-cols-11 gap-8 p-8">
-        <div className="h-96 rounded-2xl shadow-lg bg-red-200 col-span-2 -translate-y-24">
-
-        </div>
-        <div className="h-96 rounded-2xl shadow bg-green-200 col-span-6">
-
-        </div>
-        <div className="h-96 rounded-2xl shadow bg-yellow-200 col-span-3">
-
-        </div>
+        <div className="h-96 hidden md:block rounded-2xl shadow-lg bg-red-200 md:col-span-2 -translate-y-24 col-span-11"></div>
+        <div className="h-96 rounded-2xl shadow bg-green-200 md:col-span-6 col-span-11"></div>
+        <div className="h-96 rounded-2xl shadow bg-yellow-200 md:col-span-3 col-span-11"></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
